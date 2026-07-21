1689a0d9248d45560c4740fcd891ccd347f6deba
prathameshkhandare <prathameshkhandare15@gmail.com>
2026-07-21 23:46:39 +0530

    update: replace Panca-tattva Kirtana with Harinaam sankirtan schedule in blogs

--- app/blog/sivananda-sena/BlogClient.tsx
+++ app/blog/sivananda-sena/BlogClient.tsx
@@ -824,7 +824,7 @@ export default function BlogClient() {
                         <li>Morning Arati 4:30 AM</li>
                         <li>Bhagavatam Class 8.05 AM</li>
                         <li>Evening Arati 7:00 PM</li>
-                        <li>Panca-tattva Kirtana Daily</li>
+                        <li>Harinaam sankirtan 7.35 AM</li>
                     </ul>
                 </div>
 
